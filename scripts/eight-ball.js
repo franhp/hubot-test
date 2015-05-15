14a582f824894c67b17694f07f7a2f54a04c64ed
James Hardy <james@Jamess-MacBook-Pro-2.local>
2015-05-15 14:21:07 +0100

    I'm a bad man

--- scripts/eight-ball.js
+++ scripts/eight-ball.js
@@ -1,19 +1,19 @@
 /*Description:
-*
-*   Allows Hubot to use a magic 8-ball
-*
-* Dependencies:
-*   None
-*
-* Configuration:
-*   None
-*
-* Commands:
-*   hubot [8ball|8 ball|8-ball] <question>
-*
-* Author:
-*   james hardy
-*/
+ *
+ *   Allows Hubot to use a magic 8-ball
+ *
+ * Dependencies:
+ *   None
+ *
+ * Configuration:
+ *   None
+ *
+ * Commands:
+ *   hubot [8ball|8 ball|8-ball] <question>
+ *
+ * Author:
+ *   james hardy
+ */
 
 module.exports = function ( robot ) {
     robot.respond( /8[ \-]?ball (.*)/i, function ( msg ) {
@@ -23,23 +23,37 @@ module.exports = function ( robot ) {
             "Without a doubt",
             "Yes definitely",
             "You may rely on it",
+            "Yes",
             "As I see it, yes",
             "Most likely",
             "Outlook good",
-            "Yes",
             "Signs point to yes",
             "Reply hazy try again",
             "Ask again later",
             "Better not tell you now",
             "Cannot predict now",
             "Concentrate and ask again",
+            "Outlook not so good",
+            "Very doubtful",
             "Don't count on it",
             "My reply is no",
-            "My sources say no",
-            "Outlook not so good",
-            "Very doubtful"
+            "My sources say no"
         ];
-        var ix = Math.floor( Math.random() * 20 );
-        msg.reply( 'I asked the Magic 8-Ball "' + msg.match[1] + '"\nIt said "' + responses[ ix ] + '"' );
-    });
+        var ix;
+        var question = msg.match[ 1 ];
+        
+        //Because sometimes I just want someone to agree with me
+        if ( msg.user.name.toLowerCase() === 'mrweeble' ) {
+            if ( /isn't [^ ]+$/.test( question ) ) {
+                ix = Math.floor( Math.random() * 7 );
+            } else if ( /is [^ ]+$/.test( question ) ) {
+                ix = Math.floor( Math.random() * 4 ) + 16;
+            } else {
+                ix = Math.floor( Math.random() * 20 );
+            }
+        } else {
+            ix = Math.floor( Math.random() * 20 );
+        }
+        msg.reply( 'I asked the Magic 8-Ball "' + msg.match[ 1 ] + '"\nIt said "' + responses[ ix ] );
+    } );
 };
